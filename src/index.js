36756e3ecf36c38b6353b68867c703387af955ac
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2019-02-19 23:26:59 +0100

    Added hook code

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import {useState, useEffect} from 'react'
+
+const useDeviceOrientation = () => {
+  const [orientation, setOrientation] = useState({
+    absolute: null,
+    alpha: null,
+    beta: null,
+    gamma: null
+  })
+
+  const handleOrientationChange = ({
+    absolute = null,
+    alpha = null,
+    beta = null,
+    gamma = null
+  }) => {
+    setOrientation({absolute, alpha, beta, gamma})
+  }
+
+  useEffect(() => {
+    window.addEventListener('deviceorientation', handleOrientationChange, true)
+    return () => {
+      window.removeEventListener('deviceorientation', handleOrientationChange)
+    }
+  }, [])
+
+  return orientation
+}
+
+export default useDeviceOrientation
